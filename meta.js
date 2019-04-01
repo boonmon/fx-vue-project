a16de4842d84d1deecaaf85fb6f90554e62741fe
linjituan <linjituan@foresee.com.cn>
2019-04-01 09:39:31 +0800

    commit

--- meta.js
+++ meta.js
@@ -1,12 +1,12 @@
 const path = require('path')
 const fs = require('fs')
 
-const {
-  sortDependencies,
-  installDependencies,
-  runLintFix,
-  printMessage,
-} = require('./utils')
+// const {
+//   sortDependencies,
+//   installDependencies,
+//   runLintFix,
+//   printMessage,
+// } = require('./utils')
 const pkg = require('./package.json')
 
 const templateVersion = pkg.version
@@ -177,27 +177,27 @@ module.exports = {
     // 'src/page/login/*': 'login',
     // 'src/utils/loginUtil.js': 'login'
   },
-  complete: function (data,
-    { chalk }) {
-    const green = chalk.green
+  // complete: function (data,
+  //   { chalk }) {
+  //   const green = chalk.green
 
-    sortDependencies(data, green)
+  //   sortDependencies(data, green)
 
-    const cwd = path.join(process.cwd(), data.inPlace ? '' : data.destDirName)
+  //   const cwd = path.join(process.cwd(), data.inPlace ? '' : data.destDirName)
 
-    if (data.autoInstall) {
-      installDependencies(cwd, data.autoInstall, green)
-        .then(() => {
-          return runLintFix(cwd, data, green)
-        })
-        .then(() => {
-          printMessage(data, green)
-        })
-        .catch(e => {
-          console.log(chalk.red('Error:'), e)
-        })
-    } else {
-      printMessage(data, chalk)
-    }
-  },
+  //   if (data.autoInstall) {
+  //     installDependencies(cwd, data.autoInstall, green)
+  //       .then(() => {
+  //         return runLintFix(cwd, data, green)
+  //       })
+  //       .then(() => {
+  //         printMessage(data, green)
+  //       })
+  //       .catch(e => {
+  //         console.log(chalk.red('Error:'), e)
+  //       })
+  //   } else {
+  //     printMessage(data, chalk)
+  //   }
+  // },
 }
